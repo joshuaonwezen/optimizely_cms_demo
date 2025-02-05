bc6514787bb63b26f0d421a1f65bd8cd2c893a00
Joshua Onwezen <joshua.onwezen@gmail.com>
2025-02-05 09:11:25 +0100

    doublecheck return query

--- src/components/base/VisualBuilderComponent.tsx
+++ src/components/base/VisualBuilderComponent.tsx
@@ -6,170 +6,179 @@ import CompositionNodeComponent from "./CompositionNodeComponent";
 import { onContentSaved } from "@/helpers/onContentSaved";
 
 export const VisualBuilder = graphql(/* GraphQL */ `
-  query VisualBuilder($url: String, $key: String, $version: String) {
-    _Experience(
-      where: {
-        _or: [
-          {
-            _and: [
-              { _metadata: { url: { default: { eq: $url } } } },
-            ]
-          },
-          {
-            _and: [
-              { _metadata: { key: { eq: $key } } },
-              { _metadata: { version: { eq: $version } } }
-            ]
-          }
-        ]
-      }
-    ) {
-      items {
-        composition {
-          grids: nodes {
-            ... on CompositionStructureNode {
-              key
-              displaySettings {
-                key
-                value
-              }
-              rows: nodes {
-                ... on CompositionStructureNode {
-                  key
-                  columns: nodes {
-                    ... on CompositionStructureNode {
-                      key
-                      elements: nodes {
-                        ...compositionComponentNode
-                      }
+    query VisualBuilder($url: String, $key: String, $version: String) {
+        _Experience(
+            where: {
+                _or: [
+                    {
+                        _and: [
+                            { _metadata: { url: { default: { eq: $url } } } }
+                        ]
                     }
-                  }
-                }
-              }
+                    {
+                        _and: [
+                            { _metadata: { key: { eq: $key } } }
+                            { _metadata: { version: { eq: $version } } }
+                        ]
+                    }
+                ]
             }
-          }
+        ) {
+            items {
+                composition {
+                    grids: nodes {
+                        ... on CompositionStructureNode {
+                            key
+                            displaySettings {
+                                key
+                                value
+                            }
+                            rows: nodes {
+                                ... on CompositionStructureNode {
+                                    key
+                                    columns: nodes {
+                                        ... on CompositionStructureNode {
+                                            key
+                                            elements: nodes {
+                                                ...compositionComponentNode
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                _metadata {
+                    key
+                    version
+                }
+            }
         }
-        _metadata {
-          key
-          version
-        }
-      }
     }
-  }
 `);
 
 interface VisualBuilderProps {
-  contentKey?: string;
-  version?: string;
-  url?: string;
+    contentKey?: string;
+    version?: string;
+    url?: string;
 }
 
 const VisualBuilderComponent: FC<VisualBuilderProps> = ({
-  version,
-  contentKey,
-  url,
+    version,
+    contentKey,
+    url,
 }) => {
-  const variables: Record<string, unknown> = {};
-  if (version) {
-    variables.version = version;
-  }
-
-  if (contentKey) {
-    variables.key = contentKey;
-  }
-
-  if (url) {
-    variables.url = url;
-  }
-
-  console.log(variables)
-  const { data, refetch, error } = useQuery(VisualBuilder, {
-    variables: variables,
-    notifyOnNetworkStatusChange: true,
-  });
-  if (error) {
-    console.error("GraphQL Error:", error.message);
-  }
-
-  useEffect(() => {
-    if (data) {
-      console.log("GraphQL response data:", data);
-    }
-  }, [data]);
-
-  if (error) {
-    console.error("GraphQL Error:", error.message);
-  }
-
-  useEffect(() => {
-    onContentSaved((_) => {
-      const contentIdArray = _.contentLink.split("_");
-      if (contentIdArray.length > 1) {
-        version = contentIdArray[contentIdArray.length - 1];
+    const variables: Record<string, unknown> = {};
+    if (version) {
         variables.version = version;
-      }
-      refetch(variables);
+    }
+
+    if (contentKey) {
+        variables.key = contentKey;
+    }
+
+    if (url) {
+        variables.url = url;
+    }
+
+    console.log(variables);
+    const { data, refetch, error } = useQuery(VisualBuilder, {
+        variables: variables,
+        notifyOnNetworkStatusChange: true,
     });
-  }, []);
+    if (error) {
+        console.error("GraphQL Error:", error.message);
+    }
 
-  const experiences = data?._Experience?.items;
-  if (!experiences) {
-    return null;
-  }
+    useEffect(() => {
+        if (data) {
+            console.log("GraphQL response data:", data);
+        }
+    }, [data]);
 
-  const experience: any = experiences[experiences.length - 1];
+    if (error) {
+        console.error("GraphQL Error:", error.message);
+    }
 
-  if (!experience) {
-    return null;
-  }
+    useEffect(() => {
+        onContentSaved((_) => {
+            const contentIdArray = _.contentLink.split("_");
+            if (contentIdArray.length > 1) {
+                version = contentIdArray[contentIdArray.length - 1];
+                variables.version = version;
+            }
+            refetch(variables);
+        });
+    }, []);
 
-  return (
-    <div className="relative w-full flex-1 vb:outline">
-      <div className="relative w-full flex-1 vb:outline">
-        {experience?.composition?.grids?.map((grid: any) => {
-          const displaySetting = grid.displaySettings?.find(
-            (setting: any) => setting.key === "defaultBlogStyles"
-          );
+    const experiences = data?._Experience?.items;
+    if (!experiences) {
+        return null;
+    }
 
-          // Determine the class based on the display setting's value
-          const flexDirection =
-            displaySetting?.value === "Row" ? "flex-row" : "flex-col";
-          return (
-            <div
-              key={grid.key}
-              className={`relative w-full flex ${flexDirection} flex-colflex-nowrap justify-start vb:grid`}
-              data-epi-block-id={grid.key}
-            >
-              {grid.rows?.map((row: any) => (
-                <div
-                  key={row.key}
-                  className="flex-1 flex flex-row flex-nowrap justify-start vb:row"
-                >
-                  {row.columns?.map((column: any) => (
-                    <div
-                      className="flex-1 flex flex-col flex-nowrap justify-start vb:col"
-                      key={column.key}
-                    >
-                      {column.elements?.map((element: any) => (
+    const experience = experiences.find(
+        (exp) =>
+            exp?._metadata.key === contentKey &&
+            exp?._metadata.version === version
+    );
+    console.log(experiences, experience)
+
+    if (!experience) {
+        return null;
+    }
+
+    return (
+        <div className="relative w-full flex-1 vb:outline">
+            <div className="relative w-full flex-1 vb:outline">
+                {experience?.composition?.grids?.map((grid: any) => {
+                    const displaySetting = grid.displaySettings?.find(
+                        (setting: any) => setting.key === "defaultBlogStyles"
+                    );
+
+                    // Determine the class based on the display setting's value
+                    const flexDirection =
+                        displaySetting?.value === "Row"
+                            ? "flex-row"
+                            : "flex-col";
+                    return (
                         <div
-                          data-epi-block-id={element?.key}
-                          key={element?.key}
-                        >
-                          <CompositionNodeComponent
-                            compositionComponentNode={element}
-                          />
+                            key={grid.key}
+                            className={`relative w-full flex ${flexDirection} flex-colflex-nowrap justify-start vb:grid`}
+                            data-epi-block-id={grid.key}>
+                            {grid.rows?.map((row: any) => (
+                                <div
+                                    key={row.key}
+                                    className="flex-1 flex flex-row flex-nowrap justify-start vb:row">
+                                    {row.columns?.map((column: any) => (
+                                        <div
+                                            className="flex-1 flex flex-col flex-nowrap justify-start vb:col"
+                                            key={column.key}>
+                                            {column.elements?.map(
+                                                (element: any) => (
+                                                    <div
+                                                        data-epi-block-id={
+                                                            element?.key
+                                                        }
+                                                        key={element?.key}>
+                                                        <CompositionNodeComponent
+                                                            compositionComponentNode={
+                                                                element
+                                                            }
+                                                        />
+                                                    </div>
+                                                )
+                                            )}
+                                        </div>
+                                    ))}
+                                </div>
+                            ))}
                         </div>
-                      ))}
-                    </div>
-                  ))}
-                </div>
-              ))}
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default VisualBuilderComponent;
