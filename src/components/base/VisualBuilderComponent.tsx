534a4a913d5435c641a876a8682e10eeb9f04a65
Joshua Onwezen <joshua.onwezen@gmail.com>
2025-02-25 12:52:54 +0100

    cleaned up code

--- src/components/base/VisualBuilderComponent.tsx
+++ src/components/base/VisualBuilderComponent.tsx
@@ -198,105 +198,81 @@ const VisualBuilderComponent: FC<VisualBuilderProps> = ({
         });
     }, []);
 
-    const experiences = data?._Experience?.items;
-    const pages = data?.CityPage?.items;
+    const experiences = data?._Experience?.items ?? [];
+    const pages = data?.CityPage?.items ?? [];
 
-    // Check if at least one array exists and has items
-    if (
-        (!experiences || experiences.length === 0) &&
-        (!pages || pages.length === 0)
-    ) {
-        return null;
-    }
 
-    const experience =
-        experiences && experiences.length > 0
-            ? experiences[experiences.length - 1]
-            : null;
-    const page = pages && pages.length > 0 ? pages[pages.length - 1] : null;
+    const getLastItem = (items: any) => (items.length > 0 ? items[items.length - 1] : null);
 
-    if (!experience && !page) {
-        return null;
-    }
+    const experience = getLastItem(experiences);
+    const page = getLastItem(pages);
 
-    if (!page) {
-        return (
-            <div className="relative w-full flex-1 vb:outline">
-                <HeaderElementComponent></HeaderElementComponent>
-                {experience?.composition?.grids?.map((grid: any) => {
-                    const displaySetting = grid.displaySettings?.find(
-                        (setting: any) => setting.key === "defaultBlogStyles"
-                    );
+    if (!experience && !page) return null;
 
-                    // Determine the class based on the display setting's value
-                    const flexDirection =
-                        displaySetting?.value === "Row"
-                            ? "flex-row"
-                            : "flex-col";
-                    return (
-                        <div
-                            key={grid.key}
-                            className={`relative w-full flex ${flexDirection} flex-colflex-nowrap justify-start vb:grid`}
-                            data-epi-block-id={grid.key}>
-                            {grid.rows?.map((row: any) => (
-                                <div
-                                    key={row.key}
-                                    className="flex-1 flex flex-row flex-nowrap justify-start vb:row">
-                                    {row.columns?.map((column: any) => (
-                                        <div
-                                            className="flex-1 flex flex-col flex-nowrap justify-start vb:col"
-                                            key={column.key}>
-                                            {column.elements?.map(
-                                                (element: any) => (
-                                                    <div
-                                                        data-epi-block-id={
-                                                            element?.key
-                                                        }
-                                                        key={element?.key}>
-                                                        <CompositionNodeComponent
-                                                            compositionComponentNode={
-                                                                element
-                                                            }
-                                                        />
-                                                    </div>
-                                                )
-                                            )}
-                                        </div>
-                                    ))}
-                                </div>
-                            ))}
-                        </div>
-                    );
-                })}
-            </div>
-        );
-    } else if (
-        page &&
-        page.CityReference &&
-        page.CityReference.__typename === "CityBlock"
-    ) {
-        const cityBlock = page.CityReference;
-        const compositionComponentNode: any = {
-          component: {
-            ...cityBlock,
-          },
-        };
-        const _metadata = cityBlock._metadata;
-        if (!_metadata) {
-            return null;
-        }
+       // Render grids for an experience
+       const renderExperienceGrids = (experience: any) => (
+        <div className="relative w-full flex-1 vb:outline">
+            <HeaderElementComponent />
+            {experience?.composition?.grids?.map((grid: any) => {
+                const displaySetting = grid.displaySettings?.find(
+                    (setting: any) => setting.key === "defaultBlogStyles"
+                );
+                const flexDirection = displaySetting?.value === "Row" ? "flex-row" : "flex-col";
+
+                return (
+                    <div
+                        key={grid.key}
+                        className={`relative w-full flex ${flexDirection} flex-colflex-nowrap justify-start vb:grid`}
+                        data-epi-block-id={grid.key}
+                    >
+                        {grid.rows?.map((row: any) => (
+                            <div
+                                key={row.key}
+                                className="flex-1 flex flex-row flex-nowrap justify-start vb:row"
+                            >
+                                {row.columns?.map((column: any) => (
+                                    <div
+                                        className="flex-1 flex flex-col flex-nowrap justify-start vb:col"
+                                        key={column.key}
+                                    >
+                                        {column.elements?.map((element: any) => (
+                                            <div
+                                                data-epi-block-id={element?.key}
+                                                key={element?.key}
+                                            >
+                                                <CompositionNodeComponent
+                                                    compositionComponentNode={element}
+                                                />
+                                            </div>
+                                        ))}
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                    </div>
+                );
+            })}
+        </div>
+    );
+
+    // Render a page with a city reference
+    const renderCityPage = (cityBlock: any) => {
+        const compositionComponentNode: any = { component: { ...cityBlock } };
+        const { _metadata } = cityBlock;
+
+        if (!_metadata) return null;
+
         return (
             <div className="relative w-full flex-1 vb:outline">
                 <HeaderElementComponent />
                 <div
                     key={_metadata.key}
                     className="relative w-full flex flex-row flex-colflex-nowrap justify-start vb:grid"
-                    data-epi-block-id={_metadata.key}>
+                    data-epi-block-id={_metadata.key}
+                >
                     <div className="flex-1 flex flex-row flex-nowrap justify-start vb:row">
                         <div className="flex-1 flex flex-col flex-nowrap justify-start vb:col">
-                            <div
-                                data-epi-block-id={_metadata.key}
-                                key={_metadata.key}>
+                            <div data-epi-block-id={_metadata.key} key={_metadata.key}>
                                 <CompositionNodeComponent
                                     compositionComponentNode={compositionComponentNode}
                                 />
@@ -306,7 +282,19 @@ const VisualBuilderComponent: FC<VisualBuilderProps> = ({
                 </div>
             </div>
         );
+    };
+
+    // Main render logic
+    if (experience) return renderExperienceGrids(experience);
+
+    if (
+        page?.CityReference &&
+        page.CityReference.__typename === "CityBlock"
+    ) {
+        return renderCityPage(page.CityReference);
     }
+
+    return null;
 };
 
 export default VisualBuilderComponent;
