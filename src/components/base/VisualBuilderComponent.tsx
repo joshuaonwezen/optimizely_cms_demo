0792a22fc10b3845bbca2d1e0751a22526dd1bcc
Joshua Onwezen <joshua.onwezen@gmail.com>
2025-02-25 12:41:09 +0100

    previous format

--- src/components/base/VisualBuilderComponent.tsx
+++ src/components/base/VisualBuilderComponent.tsx
@@ -1,11 +1,12 @@
 import React, { FC, useEffect } from "react";
 import { useQuery } from "@apollo/client";
+
 import { graphql } from "@/graphql";
 import CompositionNodeComponent from "./CompositionNodeComponent";
 import { onContentSaved } from "@/helpers/onContentSaved";
 import HeaderElementComponent from "../elements/HeaderElementComponent";
 
-export const VisualBuilderQuery = graphql(/* GraphQL */ `
+export const VisualBuilder = graphql(/* GraphQL */ `
     query VisualBuilder($url: String) {
         _Experience(where: { _metadata: { url: { default: { eq: $url } } } }) {
             items {
@@ -63,7 +64,7 @@ export const VisualBuilderQuery = graphql(/* GraphQL */ `
     }
 `);
 
-export const PreviewBuilderQuery = graphql(/* GraphQL */ `
+export const PreviewBuilder = graphql(/* GraphQL */ `
     query PreviewBuilder($key: String, $version: String) {
         _Experience(
             where: {
@@ -142,73 +143,124 @@ interface VisualBuilderProps {
     url?: string;
 }
 
-const VisualBuilderComponent: FC<VisualBuilderProps> = ({ version, contentKey, url }) => {
-    const variables: Record<string, unknown> = {
-        ...(version && { version }),
-        ...(contentKey && { key: contentKey }),
-        ...(url && { url }),
-    };
+const VisualBuilderComponent: FC<VisualBuilderProps> = ({
+    version,
+    contentKey,
+    url,
+}) => {
+    const variables: Record<string, unknown> = {};
+    if (version) {
+        variables.version = version;
+    }
 
-    const isPreview = !!variables.key;
-    const { data, refetch, error } = useQuery(isPreview ? PreviewBuilderQuery : VisualBuilderQuery, {
-        variables,
-        notifyOnNetworkStatusChange: true,
-    });
+    if (contentKey) {
+        variables.key = contentKey;
+    }
+
+    if (url) {
+        variables.url = url;
+    }
+
+    // Check if variables.key is defined
+    const isPreview = variables.key !== undefined;
+
+    // Select the correct query based on preview mode
+    const { data, refetch, error } = useQuery(
+        isPreview ? PreviewBuilder : VisualBuilder,
+        {
+            variables: variables,
+            notifyOnNetworkStatusChange: true,
+        }
+    );
+    if (error) {
+        console.error("GraphQL Error:", error.message);
+    }
 
     useEffect(() => {
-        if (data) console.log("GraphQL response data:", data);
+        if (data) {
+            console.log("GraphQL response data:", data);
+        }
     }, [data]);
-
-    useEffect(() => {
-        onContentSaved((event) => {
-            const contentIdArray = event.contentLink.split("_");
-            if (contentIdArray.length > 1) {
-                const newVersion = contentIdArray[contentIdArray.length - 1];
-                refetch({ ...variables, version: newVersion });
-            }
-        });
-    }, [refetch]);
 
     if (error) {
         console.error("GraphQL Error:", error.message);
+    }
+
+    useEffect(() => {
+        onContentSaved((_) => {
+            const contentIdArray = _.contentLink.split("_");
+            if (contentIdArray.length > 1) {
+                version = contentIdArray[contentIdArray.length - 1];
+                variables.version = version;
+            }
+            console.log(variables, isPreview);
+            refetch(variables);
+        });
+    }, []);
+
+    const experiences = data?._Experience?.items;
+    const pages = data?.CityPage?.items;
+
+    // Check if at least one array exists and has items
+    if (
+        (!experiences || experiences.length === 0) &&
+        (!pages || pages.length === 0)
+    ) {
         return null;
     }
 
-    const experiences = data?._Experience?.items || [];
-    const pages = data?.CityPage?.items || [];
+    const experience =
+        experiences && experiences.length > 0
+            ? experiences[experiences.length - 1]
+            : null;
+    const page = pages && pages.length > 0 ? pages[pages.length - 1] : null;
 
-    if (experiences.length === 0 && pages.length === 0) return null;
-
-    const experience = experiences[experiences.length - 1] || null;
-    const page = pages[pages.length - 1] || null;
-
-    if (!experience && !page) return null;
+    if (!experience && !page) {
+        return null;
+    }
 
     if (!page) {
         return (
             <div className="relative w-full flex-1 vb:outline">
-                <HeaderElementComponent />
+                <HeaderElementComponent></HeaderElementComponent>
                 {experience?.composition?.grids?.map((grid: any) => {
                     const displaySetting = grid.displaySettings?.find(
                         (setting: any) => setting.key === "defaultBlogStyles"
                     );
 
-                    const flexDirection = displaySetting?.value === "Row" ? "flex-row" : "flex-col";
+                    // Determine the class based on the display setting's value
+                    const flexDirection =
+                        displaySetting?.value === "Row"
+                            ? "flex-row"
+                            : "flex-col";
                     return (
                         <div
                             key={grid.key}
-                            className={`relative w-full flex ${flexDirection} flex-nowrap justify-start vb:grid`}
-                            data-epi-block-id={grid.key}
-                        >
+                            className={`relative w-full flex ${flexDirection} flex-colflex-nowrap justify-start vb:grid`}
+                            data-epi-block-id={grid.key}>
                             {grid.rows?.map((row: any) => (
-                                <div key={row.key} className="flex-1 flex flex-row flex-nowrap justify-start vb:row">
+                                <div
+                                    key={row.key}
+                                    className="flex-1 flex flex-row flex-nowrap justify-start vb:row">
                                     {row.columns?.map((column: any) => (
-                                        <div className="flex-1 flex flex-col flex-nowrap justify-start vb:col" key={column.key}>
-                                            {column.elements?.map((element: any) => (
-                                                <div data-epi-block-id={element?.key} key={element?.key}>
-                                                    <CompositionNodeComponent compositionComponentNode={element} />
-                                                </div>
-                                            ))}
+                                        <div
+                                            className="flex-1 flex flex-col flex-nowrap justify-start vb:col"
+                                            key={column.key}>
+                                            {column.elements?.map(
+                                                (element: any) => (
+                                                    <div
+                                                        data-epi-block-id={
+                                                            element?.key
+                                                        }
+                                                        key={element?.key}>
+                                                        <CompositionNodeComponent
+                                                            compositionComponentNode={
+                                                                element
+                                                            }
+                                                        />
+                                                    </div>
+                                                )
+                                            )}
                                         </div>
                                     ))}
                                 </div>
@@ -218,26 +270,36 @@ const VisualBuilderComponent: FC<VisualBuilderProps> = ({ version, contentKey, u
                 })}
             </div>
         );
-    } else if (page.CityReference?.__typename === "CityBlock") {
+    } else if (
+        page &&
+        page.CityReference &&
+        page.CityReference.__typename === "CityBlock"
+    ) {
         const cityBlock = page.CityReference;
         const compositionComponentNode: any = {
-            component: { ...cityBlock },
+          component: {
+            ...cityBlock,
+          },
         };
-
-        if (!cityBlock._metadata) return null;
-
+        const _metadata = cityBlock._metadata;
+        if (!_metadata) {
+            return null;
+        }
         return (
             <div className="relative w-full flex-1 vb:outline">
                 <HeaderElementComponent />
                 <div
-                    key={cityBlock._metadata.key}
-                    className="relative w-full flex flex-row flex-nowrap justify-start vb:grid"
-                    data-epi-block-id={cityBlock._metadata.key}
-                >
+                    key={_metadata.key}
+                    className="relative w-full flex flex-row flex-colflex-nowrap justify-start vb:grid"
+                    data-epi-block-id={_metadata.key}>
                     <div className="flex-1 flex flex-row flex-nowrap justify-start vb:row">
                         <div className="flex-1 flex flex-col flex-nowrap justify-start vb:col">
-                            <div data-epi-block-id={cityBlock._metadata.key} key={cityBlock._metadata.key}>
-                                <CompositionNodeComponent compositionComponentNode={compositionComponentNode} />
+                            <div
+                                data-epi-block-id={_metadata.key}
+                                key={_metadata.key}>
+                                <CompositionNodeComponent
+                                    compositionComponentNode={compositionComponentNode}
+                                />
                             </div>
                         </div>
                     </div>
@@ -245,8 +307,6 @@ const VisualBuilderComponent: FC<VisualBuilderProps> = ({ version, contentKey, u
             </div>
         );
     }
-
-    return null;
 };
 
 export default VisualBuilderComponent;
