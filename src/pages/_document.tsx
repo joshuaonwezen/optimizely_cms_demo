b29f6f3ab055dc6b6ee749da758fb233d92ac185
Joshua Onwezen <joshua.onwezen@gmail.com>
2025-05-20 09:07:40 +0200

    Update with webscript

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,21 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preload" href="//cdn.optimizely.com/js/23338860169.js" as="script" />
+        <link rel="preconnect" href="//logx.optimizely.com" />
+        {/* Add other meta information here */}
+        {/* Add stylesheets here */}
+        
+        {/* Ensure script loads in head */}
+        <Script
+          src="//cdn.optimizely.com/js/23338860169.js"
+          strategy="beforeInteractive"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
