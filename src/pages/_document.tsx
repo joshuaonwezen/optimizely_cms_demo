5740f2a0ebcc92fc702b9f21d3ce08d8b06bb755
Joshua Onwezen <joshua.onwezen@gmail.com>
2025-06-17 12:57:26 +0200

    update favicon and script loading

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,23 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
+import { useEffect, useState } from "react";
+
+function OptimizelyScript() {
+  const [strategy, setStrategy] = useState<"beforeInteractive" | "afterInteractive">("beforeInteractive");
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.location.hostname === "localhost") {
+      setStrategy("afterInteractive");
+    }
+  }, []);
+
+  return (
+    <Script
+      src="//cdn.optimizely.com/js/23338860169.js"
+      strategy={strategy}
+    />
+  );
+}
 
 export default function Document() {
   return (
@@ -7,14 +25,13 @@ export default function Document() {
       <Head>
         <link rel="preload" href="//cdn.optimizely.com/js/23338860169.js" as="script" />
         <link rel="preconnect" href="//logx.optimizely.com" />
+        <link rel="icon" href="https://app-ocstjoshuac8je4ft001.cms.optimizely.com/Util/images/favicon.ico" />
+
         {/* Add other meta information here */}
         {/* Add stylesheets here */}
         
-        {/* Ensure script loads in head */}
-        <Script
-          src="//cdn.optimizely.com/js/23338860169.js"
-          strategy="beforeInteractive"
-        />
+        {/* Conditionally load script based on hostname */}
+        <OptimizelyScript />
       </Head>
       <body>
         <Main />
